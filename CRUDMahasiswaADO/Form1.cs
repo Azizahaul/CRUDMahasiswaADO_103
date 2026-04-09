10af0746689cb71591e6e79781b4907ea47629dc
Azizahaul <azizahauliarhamid@gmail.com>
2026-04-09 18:12:19 +0700

    tambah constructor untuk inisialisasi koneksi database

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -10,13 +10,33 @@ namespace CRUDMahasiswaADO
         private readonly string connectionString =
             "Data Source=LAPTOP-RB8DQ53H\\ZIZEE;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
+
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
+      
+        
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void ConnectDataBase()
         {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
 
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDataBase();
         }
 
         private void textBox7_TextChanged(object sender, EventArgs e)
