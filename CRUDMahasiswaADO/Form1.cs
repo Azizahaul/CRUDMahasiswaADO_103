92346feef9a6cd94fe9ea0643591e2cb9dad6e94
Azizahaul <azizahauliarhamid@gmail.com>
2026-04-09 17:52:15 +0700

    Add project files.

---
+++ CRUDMahasiswaADO/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpTanggalLahir_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
